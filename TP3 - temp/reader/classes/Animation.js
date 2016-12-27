f27f4c1e55220aee977619ce03cd82f58a139503
Alexandre Saraiva Moreira <alexandremoreira_13@sapo.pt>
2016-12-27 02:08:00 +0000

    Removing the unnecessary

--- TP3 - temp/reader/classes/Animation.js
+++
@@ -1,8 +0,0 @@
-function Animation(scene, id, span, type) {
-	this.id = id;
-	this.span = span;
-	this.type = type;
-	this.done = false;
-	this.lastTime = 0;
-	this.scene = scene;
-}
--- Teste/tp5/Point3.js
+++
@@ -1,5 +0,0 @@
-function Point3(x, y, z) {
-	this.x = x;
-	this.y = y;
-	this.z = z;
-}
